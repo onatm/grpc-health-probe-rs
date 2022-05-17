80e6a787797b55c726526d7ac4014de7848c6ade
onatm <onatmercan@gmail.com>
2022-05-17 22:54:06 +0100

    add timeout configs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{process, time::Duration};
+
 use clap::Parser;
 use tonic::{transport::Channel, Request};
 use tonic_health::proto::{health_client::HealthClient, HealthCheckRequest};
@@ -16,16 +18,36 @@ struct Flags {
     /// user-agent header value of health check requests
     #[clap(long, default_value = "grpc_health_probe", required = false)]
     user_agent: String,
+
+    /// timeout for establishing connection
+    #[clap(long, default_value_t = 1, required = false)]
+    connect_timeout: u64,
+
+    /// timeout for health check rpc
+    #[clap(long, default_value_t = 1, required = false)]
+    rpc_timeout: u64,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let flags = Flags::parse();
 
-    let channel = Channel::from_shared(flags.addr)?
+    if flags.connect_timeout == 0 {
+        println!("--connect-timeout must be greater than zero");
+        process::exit(1);
+    }
+
+    if flags.rpc_timeout == 0 {
+        println!("--rpc-timeout must be greater than zero");
+        process::exit(1);
+    }
+
+    let channel_builder = Channel::from_shared(flags.addr)?
         .user_agent(flags.user_agent)?
-        .connect()
-        .await?;
+        .connect_timeout(Duration::from_secs(flags.connect_timeout))
+        .timeout(Duration::from_secs(flags.rpc_timeout));
+
+    let channel = channel_builder.connect().await?;
 
     let mut client = HealthClient::new(channel);
 
